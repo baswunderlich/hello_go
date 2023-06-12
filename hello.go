2da22d9972b470d168b3ec07bb5c4707d101205a
baswunderlich <bastian.wunderlich@protonmail.com>
2023-06-12 11:14:13 +0200

    initial commit

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"firstModule/screams"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.Println("hello dear friend")
+	screams.Wolf()
+}
---
+++ screams/screams.go
@@ -0,0 +1,11 @@
+package screams
+
+import "fmt"
+
+func owl() {
+	fmt.Println("shu shu")
+}
+
+func Wolf(){
+	fmt.Println("ohhhhhhhh")
+}
